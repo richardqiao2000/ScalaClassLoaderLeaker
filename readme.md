5d705d043c3fdc16eb0195dbe171f849050e176c
richardqiao2000 <richardqiao2000@gmail.com>
2017-05-01 16:50:36 -0500

    udpate

---
+++ readme.md
@@ -0,0 +1,10 @@
+# Class Loader Leaker
+
+-verbose:gc
+-verbose:class
+-Xmx1024M -XX:MetaspaceSize=128m
+-XX:MaxMetaspaceSize=128m
+-XX:+PrintGCDetails
+-XX:+PrintGCTimeStamps
+-Xloggc:E:/code/scala-ide-4.50/ScalaClassLoaderLeaker/gc.log
+-Dfile.encoding=Cp1252
--- src/main/scala/org/richardqiao/memory/MemoryLeaker.scala
+++ src/main/scala/org/richardqiao/memory/MemoryLeaker.scala
@@ -8,20 +8,26 @@ object MemoryLeaker {
   private val leakingMap = new HashMap[String, TraitData]()
   private final val ITERATIONS = 5000000
   def main(args: Array[String]): Unit = {
+    metaSpaceLeaker
+  }
+  
+  def metaSpaceLeaker{
     println("Scala Class Loader Leaker")
     try{
+      //val typeSeq = "file:" + 2
       for(i <- 0 until ITERATIONS){
-        val typeSeq = "file:" + i
+        val typeSeq = "http:" + i + ".jar"
         val urls: Array[URL] = Array(new URL(typeSeq))
         val classLoader: URLClassLoader = new URLClassLoader(urls)
         val data: TraitData = Proxy.newProxyInstance(classLoader,
                                                Array[Class[_]](classOf[TraitData]),
                                                new DataInvationHandler(new Data())
                                               ).asInstanceOf[TraitData]
+        //Comment this line off to see the good behaviour of Meta Space GC
         leakingMap.put(typeSeq, data)
       }
     }catch{
-        case _ => println("error")
+        case e: Exception => e.printStackTrace
     }
   }
 }
